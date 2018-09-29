48d9c8bce99a5f5e0bd827f901392292169e054c
abdo hashem <ar1813@fayoum.edu.eg>
2018-09-30 01:18:49 +0200

    working sound generator
    added square wave and others

--- sound_make.py
+++ sound_make.py
@@ -1,15 +1,35 @@
-import struct
 import numpy as np
-from scipy import signal as sg
+from scipy.io.wavfile import write
 import os
 
-Fs = 44100                    ## Sampling Rate
-f = 440                       ## Frequency (in Hz)
-sample = 44100                ## Number of samples 
-x = np.arange(sample)
+# Samples per second
+sps = 44100
+
+# Frequency / pitch of the sine wave
+freq_hz = 440.0
+
+# Duration
+duration_s = 5.0
+
+# NumpPy magic
+each_sample_number = np.arange(duration_s * sps)
+waveform = np.sin(2 * np.pi * each_sample_number * freq_hz / sps)
+waveform_quiet = waveform * 0.3
+waveform_integers = np.int16(waveform_quiet * 32767)
+
+# Write the .wav file
+write('first_sine_wave.wav', sps, waveform_integers)
+
+os.system("aplay first_sine_wave.wav")
+
+''' other waves
+# Fs = 44100  # Sampling Rate
+# f = 440  # Frequency (in Hz)
+# sample = 44100  # Number of samples
+# x = np.arange(sample)
 
 ####### sine wave ###########
-y = 100*np.sin(2 * np.pi * f * x / Fs)
+# y = 100*np.sin(2 * np.pi * f * x / Fs)
 
 ####### square wave ##########
 # y = 100* sg.square(2 *np.pi * f *x / Fs )
@@ -19,14 +39,4 @@ y = 100*np.sin(2 * np.pi * f * x / Fs)
 
 ####### Sawtooth wave ########
 # y = 100* sg.sawtooth(2 *np.pi * f *x / Fs )
-
-
-f = open('test.wav','wb')
-## Open as Signed 8-bit on Audacity - Watch Video for instructions
-
-for i in y:
-	# print (i)
-	f.write(struct.pack('f',i))
-f.close()
-print ("playing")
-os.system("aplay test.wav")
+'''
